7a56345d90b98456379782a64343970bf12455d0
Tojo Saki <tomguts@126.com>
2015-12-06 13:09:22 +0800

    Major: 0.9
    Add Honoka x1

--- src/sdk/knversion.h
+++ src/sdk/knversion.h
@@ -24,10 +24,191 @@
  * synced from here.
  */
 
+/*
+ * Code Name: Honoka
+                      -^.^^`
+                    ./\\//!,^
+                  ´',',!!_'..--`
+                 `~`   ´^'^~´-~-
+                      `^'.---`-~ ´-
+                     `'_^~~-----'9c    `,!)^
+                    `~`  `-^.^.´0@z  .<v5Cent<`
+                            .~`}Rpqo3OaV5eOYfLl7'
+                        `´:z&3c\dK$V$S9&CuCe5Oh0z"
+                   `.))´ }OY5gBuySueLaYJJJ3u22LSaLc
+                   /=*"?)Cu$bfd@LJ3J3e3jj1CLcjllCus+
+                 `)*"'<+I58ZAKXajlIc1zr7??vcl[[17l3J~
+                  )7j?7)3sU6wRZ11]]+oo{}{7c<>clC]}[l:
+              ~'_<\\*?Ci"fZFbdJ=;7=}s=:}"[*'^^^2T*{;?^
+             >{\(?I3en5aeTDpbZ}::?"tt{=77]!,''^~8l]j?)
+            *J `  -c896669wwKS*};]ro;1lltr,'.^'-+6gOj;
+            1a` _rJ5aV$wF»b«A5+r1oo?*tCeur^\;"()/8etCi`
+           ´*h3[CYOroZnaydB#QnIjJCJ<;nYY5[.:)''/!=&Uat~
+         ´}r?i[7JL4J_]^ \o46xOJ25YJ<{nSeeI^^~.-~-j«g0C'
+        ~3u]cl+i?ohbY^´´`  ~YS&&00o'-IU&re,'*rI{`>sCL&\
+        vl_~itC?[6hc\´-~´ 'lC00Yaa=´,oTx,1}~2Th0o'u5aL>
+       !0` +LSu2%v`     ,<?)ox44L5"]%TS4".o/3!5f5u3aVY<
+       {S  3JSYU+       ´- ^T8USYI3U/a$J<'^,\Y59]t&fhO)
+       )g;)IiPT]        `!*lhO$f4;fJ}Y«;)^''.2n5,_Zxae\
+       `3l;icgt~       ;j]1&0sG$a3Y1G3Y*^''''^:!.>b5ra*
+        `?;i1S{.      :+.~\CUa$«YL,^?2*.',_'',!(,C%l"fj
+           >en]/`    !\^IO&sYOhgG5}.',,'',,^.'\);l*[Ih5
+            <o[*`´   c]YGS2e&OV«Vh2,(<!,--^')!,!\~"jaT8*                 -/
+             ]\r'    lVSf22C50%dn3wO\_,^cOC0p8-'^(tUSv2u.                _>,\_
+             '/-'`  'l\[Oune8wSOg&ls;_^~gb0L4u-'-28hf+'It~              `!>;<(
+              '((-  ;*~sO55UP1?TpP3S).'~[5llt<^~}«u$$o  *e^            `,\l]()
+               ``   }`?k%U3S<_&g»a%Ah_-'~"17*.^+w»UZ%o ´ zt           ^)\[Ci)<`
+                    )-1P9u]" 3hdxsX%GZj_--.^~_{ZpDggho ´ 7z          ´<'<[jo)<'
+                    ~!inS{> /YZ&Gp9'YqpZz"!,\7ITdg0»U3\)\J[          ^\^+i[3:_'
+                      )ts-?~:fu[GP{ 8byppncrtt]s0t8X0t'*7?^         _)\'?c1o1*^
+                      ´}e`*7+8_jO& `$b»phto3zTt3-j2Lr!!\)'         ~}j\,"::{**`
+                       `1! ~\l 73& [yZb$%f5$9Jfn[J"`´ ,_           />l:!'~.__~
+                        `<. `1 }Ie/Zq»«&6KQR5"VC&; ^,´~\          ./,{1<'()^
+                         ~_  )<\(lU»pKpqdXKp2zO{?_ -´--<^         \~.\[7+/`
+                             `(~[OPbD»ppRygwJ}~_=~´´´´-((        ,( ´/)j7^
+                             .{chppRbPpXpXcI_ ^7{ _´-´-_{        i* ´^*"c?~
+                             "?;]«dp0gRdDWL__<7r "*(--´~?)´      [; -~*}L;/
+                           (z('*_9m51»Jl56L"=;>/Cy{;!´´´;\'     rOj -."{O}(
+                          >P; ^"r9s rt";+*)\-'JwqRl-<`´~"^!    ´6Gtr !}<Ol`
+                         ^6S `-*Z6:J5+;(.`  IgqwZyw__´ !)`!.   ibbnJ]\);8(
+                         vmI ,}lydbbj"~ `` sQwGGZPbV!- <^ `/´ -9wgD]JU5%:
+                        .hq[`*75yPkh+!:(` "qkGPPPgGd%^-)```~! 1DPZy«j7e&
+                        <y#u{^/Pggy9_}j;  cqPPPPPZyZK7'\ ´´ ~,hkZgk»Rgt~
+`<(!__,~                }RE8*`Um««mP!\/.` cpZPPZZZ66R0'^`´`~-v»PZg«FRQ[
+/>]}(//(/ .´            7mBC 5QRRRmq*!~`´ =dgZZZZ«?*pKJ --)>{PgZgkpRKG`
+!*33},^.(=!_-          `1Q% aQRRFFRQe`<^` <Dwggg«T` rRm" ,(_9yGZg»RRQc
+_7tr1=',?/ ´_-        `,>»?U»GqD««XTO<.:\`'k«kgyd:   uFX=`~4»PZgkpRKZ-
+_[J[{_':*-`` <}´      !'nU4#&0«www«hZh .{+<6q»bFK[    ;xp>o«GZZg«RRQ=
+_?i\^(;;_`´ *3c"^   ~\:1GtGbwwgkkkk»DmL´´({$NRRmFL     /wXZZZZggdRQ0
+^(_(}+;>~` \vL]`-_-!"'.&j[$wyggZgggyw»We' `o@FFX»f`     lKXPZgkwqKD,
+`,,^~ c"~´~l3DbT]!(<)"1t"[xwkPZZZZgwwwm2r}.^yDykwZ/     ~wdZggg»RQz
+     `jt_`7l$gPqm«0! 'r>~=xkgPPPPZZkg»G{?[]"8qwgg«7      [Dkg»Xqmq\
+     -19n2&&DZgwZdqge{{-.?%ggZPPPPZggDV´<=7rtV%Zw»z       3bpRRRQ0
+      >cf8Oy«kgZwkGgd»a 'r%gkpDZPPZgZbY ~_*=ztcl598`       JQQmKq)
+        >4v%F«kkgZZPPwq],[%g«FRdgZZgPdC -~.iz][[rIY^        7GRD[
+         ~{(IkpykkggZPwqT3hg»RRRbZwwGdz --,tci]i1](          `*'
+             ,uyp»kgggg»KgSwgpFFdwaaZb3 -`;7\""</´
+               !3$pq«wkwDiOwCZwwgwesZDt `!ji-
+                 `}xQRpF1`8whZPPZPkgG»v /*"1~    "z<
+                    jZbo -a»wPPPPPZZGwO_!\*;[' /0dg'
+                     `´  .YbkPPPPGZZGb4.\<<({]v«AqS
+                      >)`\9bkPPPPGZZGdz~[9«k%9Qqkpy'       ~~
+                     >m2 "ZbwZPPPGggGbJ !eN@A#%<(+j'    !;_O7`
+                     7FP'>g«»PPPPGkg6bC /5BbWV  `_^ -.!!+%Y2^.
+                    -{vmC_G$5gZPPgs8gw6~'l5yd! -,^7[__'´ ?d1-~
+                   \6qKwh;kG$»PZGZ%9PGXl`?$AL`--_<}I?(_^)YOGr\!i)
+                   >XRL,a%«kyb»ZgPggPPw«{Umy}*~```-.!()/*"3S~,8h:
+                   *9=  TFXywgpDw«ywggZdmpQ3`!<\-  `      ~U1$f;/
+                   }?  .oKbk»k»mdyDXdXRucDZ*'~~!<),~   `~_.YFv-!^
+                       !,S>?Z»ybFq»»pFe! .ii^~~`´'\*}:"+>!nx%) /´
+                      '*\\  )sw««dpp8"~((`"I[^´-´` `^\*;;0h_]V´_
+                     -)^~}I' `}u8ae7. ~<ri=*]l<´`-´´`   ^"8)_L}_
+               ~_    /^´-:l=!~`´!```\)^<c}*+>;31\-`´--´´` Yc _Oh?~
+              `!?j-`~) `!?+]]\^*( _,^c]+={!:<"{3j+*_-´--- ?826a?,\-
+              ~_`&7cn) ,:</`*rcc!´3))t[}*"",{!:>l]}][+<^^78wY*  ´'
+              /{\cZ5:,_<),``~1]]<\t*")!>>)"!\*'"_r?);]cn«»C%* ´`_´
+             `{=ikp;`/!/^`` '_>"]Uz3+´~)<>_<.<\^{'*;(*{]st\Tu--~_
+             _7vGl[Oj}{!´~~`(U$i]03&w7-\\;,,\-{_!=,_\'(">7itY´-_-
+             ?7/On[6i,)=;". OY=_,.^~;0"__?)~'\-:'!?\~,'.*"%Xf~~!
+             }r;)%b" ``~{)`!i,!_~~^^ *2?-}='´_\´:!'=*~_*.J@E&!_'
+            ^\r}Cdfa~`-.)-`7</._]Yfh5\ew-<r\^´/(`:).;;,_"3bo`*?´
+           ~!/;]qC*VS,---~=+!^]Gpgxfw»fU-':<'_ ))`>>~{7^<FZ  -\
+           /\<{9O!;"uzrr?][\.2Xf=,.^)vmD- "!/\.'}))5+~("vHo
+             '{j-`,\716D17}^vf:_{:.,\_zmw}(!\^\~*}eEO,uP6J~
+             ^.    rrhBQ17<es~^^I7~^/;!OHJ-"~!_,^}4BQn»@»\
+                   ~[««QJ}Og_~(r3r~'^^-rRV =`)~<~.z#KRN«)`
+                    rG%QO3P2(!^;\\''''^\kEJ;h*"yY*f#D»e"
+                    2k$KksV{!:..^^'''''_uFB«E90mBKXDqw)
+                    adXQ#L0<:*^'''''''',?SKpd@MdRRmyTc
+                    aRKQ#9u)+\^''''''''^[1uZdXGdV9O3;'
+                    +FANA@C'_''''''''''^7[;Ia0[t??;>>~
+                    \m@E##O!_,'''''''''.[ir?;;7?7</'"`
+                    [ANWEQP<!_'''''''''~?i7rr[r}\''!*
+                    0QEHWNk{!!,''''''''~;1r7=:(,','\_
+                   /«FBMER&j"!_''''''''~;?*)/,,,,'^(~
+                   lXXQAEx<O[_!,'''''''.{>!!__,'''')
+                   abbQAE4:]57,_'''''''^<)!!_,''''_!
+                  `f»»QANO=;8z/_,''''''.*<!_,''''^\^
+                  ´9wyKANz;]5l;_,''''''.**,_,''''^(`
+                  `%yg#NB]:J&ji/,''''''.*"','''''_!
+                   amb@HK,/iun1>_''''''.">,,''''^\~
+                   7@XQHZ ``_o]+!_,''''^+\,,'''''\
+                   `SNNE=     }[<!/)>*)/"__''''^/'
+                    ´z8=      ´7;*=r[r7}\_,''''^\`
+                               'r[r7r]1"__,''''__
+                                ^{]i;c;_!_,'''^/~
+                                  ~^`<"!!_,''''/
+                                     {*!__'''^!'
+                                     :>!_,'''^(-
+                                     "<!_,''',/
+                                     {<__,''^\'
+                                     :<__,'''"`
+                                     +\__'''!(
+                                    -?_,'''^).
+                                    '?\<(,''<`
+                                    ~==[[)'!)
+                                     ;rrr(*\)
+                                     ?[r?)>\>
+                                     {][},'_/
+                                     )][)''_)
+                                     "*<,,''"`
+                                     }\__,'^:-
+                                    ~:,__,'~)~
+                                    }e"_^~~\1-
+                                   ´1$45l13aa^
+                                   !,L%88TVfu/
+                                  ',\^I8fY2{`!
+                                 _l~(" {s< ^\,´
+                                \:2?~;!%A3>}^<"
+                                /+?8,,zWH4<'-J;!
+                                `}_cfVBNN6´<e[r/
+                                 ^o6HHMMEF&5?"7`
+                                 !dKB@@@NW8/}:\
+                                 ~/s@N#QNEpv=<
+                                   0m###B@@@"
+                                   8K@B##KAHs
+                                   S@@Q@#pQg5
+                                   SKQQA#pR(
+                                   fQ#QARp6
+                                  `xQ@Q@Xq2
+                                  ~6Q@@Kbb"
+                                  'Z@#NAX$-
+                                  \wKQ@@Fu
+                                  <»QQ#Fd;
+                                  *bQQ#qP~
+                                  +p#@KFO
+                                  ]#@##Rr
+                                  3AAAQy,
+                                  aQA#K0
+                                 ´9QQ#R]
+                                 _w#BA«'
+                                 :m@@MS
+                                 jAAQW3
+                                 uKA##1
+                                 8QA#K[
+                                -P#AmR}
+                                (b##Kp<
+                                [FmAFD_
+                                8d«bqk-
+                               *««««Xh
+                               5by««d0
+                              /Gw««yb&
+                              2gg««wbz
+                             \%6w««wDI
+                             t96y««kdr
+                            -0fG«««wb"
+                            *$SZD«y«»'
+                            [D6DK»wpx
+                            ?myqQRbEv
+                            'RKpK#HD.
+                             2HEWHH+
+                              JbXg+
+*/
+
 //Change the version here.
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 9
-#define PATCH_VERSION 2
+#define PATCH_VERSION 3
 
 //----DON'T TOUCH BELOW---
 //Generate string for version.
