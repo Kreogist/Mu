630bd19d703ae155495a9a5664b600856becd6de
Saki <tomguts@126.com>
2017-07-07 12:22:01 +1000

    Major: 0.9.428 Updates: 1. Update the compile dependent.

---
+++ debian/deploy_ppa.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Function: check_package
+# Call: check_package "Package Name"
+# Check whether the package is in the apt packaging system.
+function check_package()
+{
+	echo -n "[F] Checking $1 package..."
+	check_package=$(dpkg -l | grep "ii  $1")
+	if [ "$check_package" == "" ]
+	then
+		echo ""
+		echo "[S] Installing $1..."
+		sudo apt-get install "$1"
+		echo "[I] Done: $1 has been installed."
+	else
+		echo "Done"
+	fi
+}
+
+# Check devscripts
+check_package "devscripts"
+# Launch debuild
+python3 deploy_ppa.py $@
